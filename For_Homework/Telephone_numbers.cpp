0826e2c759bec65d4bad2e6079e28eaaf0286950
ghostik89 <taras320@mail.ru>
2017-12-23 19:21:20 +0300

    I wrote to do list for telephone_numbers

---
+++ For_Homework/Telephone_numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string true_number, code, num, foo_str;
+	char ch;
+	cin >> true_number;
+	//извлечь истинный код
+	//извлечь номер
+	for (int i = 1; i <= 3; i++) {
+		cin >> foo_str;
+		//убрать у строки первые символы типа +7 или 8
+		//извлечь код
+		//извлечь номер
+		//сравнить его с исходным
+
+	}
+}
